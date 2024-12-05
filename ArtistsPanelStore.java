deba38536cb5a9362ca9c82c57d5c43509455eeb
galgalar <133832079+galgalar@users.noreply.github.com>
2024-12-05 05:35:06 -0800

    Add files via upload

---
+++ ArtistsPanelStore.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.util.*;
+
+public interface ArtistsPanelStore {
+
+    ArrayList<JPanel> digitalPanel = new ArrayList<>();
+    ArrayList<JPanel> charcoalPanel = new ArrayList<>();
+    ArrayList<JPanel> landscapesPanel = new ArrayList<>();
+    ArrayList<JPanel> portraitPanel = new ArrayList<>();
+    
+}
